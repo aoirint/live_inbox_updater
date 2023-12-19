823b262b0e60ff1ce20c65327a65986f1b34fa97
aoirint <aoirint@gmail.com>
2023-12-19 12:39:08 +0900

    live_inbox_utility: enable_users

--- live_inbox_updater/live_inbox_utility/__init__.py
+++ live_inbox_updater/live_inbox_utility/__init__.py
@@ -1,7 +1,9 @@
 from .add_users import add_users
 from .disable_users import disable_users
+from .enable_users import enable_users
 
 __all__ = [
     "add_users",
     "disable_users",
+    "enable_users",
 ]
--- live_inbox_updater/subcommand/subcommand_enable_user.py
+++ live_inbox_updater/subcommand/subcommand_enable_user.py
@@ -4,31 +4,10 @@ from logging import getLogger
 from pydantic import BaseModel
 
 from ..app_config import AppConfig
-from ..live_inbox_api.niconico_user_manager import (
-    LiveInboxApiNiconicoUserEnabledUpdateObject,
-    LiveInboxApiNiconicoUserManager,
-    NiconicoUserHasuraManager,
-)
+from ..live_inbox_api.niconico_user_manager import NiconicoUserHasuraManager
+from ..live_inbox_utility import enable_users
 
 logger = getLogger(__name__)
-
-
-def enable_users(
-    remote_niconico_user_ids: list[str],
-    niconico_user_manager: LiveInboxApiNiconicoUserManager,
-) -> None:
-    update_objects: list[LiveInboxApiNiconicoUserEnabledUpdateObject] = []
-    for remote_niconico_user_id in remote_niconico_user_ids:
-        update_objects.append(
-            LiveInboxApiNiconicoUserEnabledUpdateObject(
-                remote_niconico_user_id=remote_niconico_user_id,
-                enabled=True,
-            ),
-        )
-
-    niconico_user_manager.bulk_update_user_enabled(
-        update_objects=update_objects,
-    )
 
 
 class SubcommandEnableUserArguments(BaseModel):
