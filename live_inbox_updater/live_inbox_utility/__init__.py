e3551ff49ab3bfcf7d6f370fd5840a6156155c59
aoirint <aoirint@gmail.com>
2023-12-19 12:41:17 +0900

    live inbox utility: fetch_uncached_niconico_user_icons

--- live_inbox_updater/live_inbox_utility/__init__.py
+++ live_inbox_updater/live_inbox_utility/__init__.py
@@ -1,9 +1,11 @@
 from .add_users import add_users
 from .disable_users import disable_users
 from .enable_users import enable_users
+from .fetch_uncached_niconico_user_icons import fetch_uncached_niconico_user_icons
 
 __all__ = [
     "add_users",
     "disable_users",
     "enable_users",
+    "fetch_uncached_niconico_user_icons",
 ]
--- live_inbox_updater/subcommand/subcommand_update.py
+++ live_inbox_updater/subcommand/subcommand_update.py
@@ -1,6 +1,4 @@
-import hashlib
 import time
-import uuid
 from argparse import ArgumentParser, Namespace
 from datetime import datetime, timezone
 from logging import getLogger
@@ -16,95 +14,24 @@ from ..live_inbox_api.niconico_live_program_manager import (
 )
 from ..live_inbox_api.niconico_user_icon_cache_metadata_manager import (
     LiveInboxApiNiconicoUserIconCacheMetadataHasuraManager,
-    LiveInboxApiNiconicoUserIconCacheMetadataManager,
 )
 from ..live_inbox_api.niconico_user_icon_cache_storage_manager import (
     LiveInboxApiNiconicoUserIconCacheStorageFileManager,
-    LiveInboxApiNiconicoUserIconCacheStorageManager,
 )
 from ..live_inbox_api.niconico_user_manager import (
     LiveInboxApiNiconicoUserManager,
     NiconicoUserHasuraManager,
 )
+from ..live_inbox_utility import fetch_uncached_niconico_user_icons
 from ..niconico_api.niconico_user_broadcast_history_client import (
     NiconicoApiNiconicoUserBroadcastHistoryClient,
     NiconicoApiNiconicoUserBroadcastHistoryNiconicoClient,
 )
 from ..niconico_api.niconico_user_icon_client import (
-    NiconicoApiNiconicoUserIconClient,
     NiconicoApiNiconicoUserIconNiconicoClient,
 )
 
 logger = getLogger(__name__)
-
-
-def fetch_uncached_niconico_user_icons(
-    niconico_user_manager: LiveInboxApiNiconicoUserManager,
-    niconico_user_icon_client: NiconicoApiNiconicoUserIconClient,
-    niconico_user_icon_cache_metadata_manager: LiveInboxApiNiconicoUserIconCacheMetadataManager,
-    niconico_user_icon_cache_storage_manager: LiveInboxApiNiconicoUserIconCacheStorageManager,
-) -> None:
-    """
-    未取得のユーザアイコンを取得する
-    """
-
-    # ユーザアイコンURLリストを取得
-    niconico_users = niconico_user_manager.get_all()
-    enabled_niconico_users = list(
-        filter(lambda niconico_user: niconico_user.enabled, niconico_users),
-    )
-
-    icon_urls: set[str] = set()
-    for niconico_user in enabled_niconico_users:
-        if niconico_user.icon_url is None:
-            continue
-
-        icon_urls.add(niconico_user.icon_url)
-
-    # 取得済みのユーザアイコンURLリストを取得
-    icon_cache_metadatas = niconico_user_icon_cache_metadata_manager.get_by_urls(
-        urls=icon_urls,
-    )
-    cached_icon_urls: set[str] = set()
-    for icon_cache_metadata in icon_cache_metadatas:
-        cached_icon_urls.add(icon_cache_metadata.url)
-
-    # TODO: 取得済みのユーザアイコンが消滅していないか確認
-
-    # 未取得のユーザアイコンURLリストを作成
-    uncached_icon_urls = icon_urls - cached_icon_urls
-    logger.info(f"Found {len(uncached_icon_urls)} uncached niconico user icons")
-
-    for icon_url in uncached_icon_urls:
-        file_key = str(uuid.uuid4())
-        fetched_at = datetime.now(tz=timezone.utc)
-
-        # ユーザアイコンを取得
-        niconico_user_icon = niconico_user_icon_client.get(url=icon_url)
-
-        # ユーザアイコンを保存
-        niconico_user_icon_cache_storage_manager.save(
-            file_key=file_key,
-            content_type=niconico_user_icon.content_type,
-            content=niconico_user_icon.content,
-        )
-
-        file_size = len(niconico_user_icon.content)
-        hash_md5 = hashlib.md5(niconico_user_icon.content).hexdigest()
-
-        # ユーザアイコンのメタデータを保存
-        niconico_user_icon_cache_metadata_manager.save(
-            url=icon_url,
-            fetched_at=fetched_at,
-            file_size=file_size,
-            hash_md5=hash_md5,
-            content_type=niconico_user_icon.content_type,
-            file_key=file_key,
-        )
-
-        time.sleep(1)
-
-    logger.info(f"Fetched {len(uncached_icon_urls)} niconico user icons")
 
 
 def update_niconico_live_programs(
